8d4f8849084a428ac5ace03d116ad3c4ad0c330d
WIN\Shindarev Nikita <nshindarev@gmail.com>
2017-05-29 02:44:45 +0300

    initial library commit

---
+++ vk-sea-lib-test/CreateSocialGraph.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vk_sea_lib_test
+{
+    class CreateSocialGraph
+    {
+        private string access_token;
+        private string user_id;
+
+        //обучающая выборка, функция и результирующий граф 
+        private DataTable trainingDataset;
+        public Func<double[], int> func;
+
+
+        public CreateSocialGraph (string access_token, string user_id)
+        {
+            this.access_token = access_token;
+            this.user_id = user_id;
+        }
+
+        public void parseTrainingDataset()
+        {
+            //собираем обучающую выборку
+            CollectingTrainingDataset collector = new CollectingTrainingDataset(this.access_token, this.user_id);
+            collector.parseInformation();
+            this.trainingDataset = collector.training_dataset;
+
+            //обучаем классификатор
+            DecisionTreeBuilder dt = new DecisionTreeBuilder(this.trainingDataset);
+            dt.studyDT();
+
+
+        }
+    }
+}
---
+++ vk-sea-lib-test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+
+namespace vk_sea_lib_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserAuthorizer auth = new UserAuthorizer();
+            auth.authorize();
+
+            CollectingTrainingDataset collectDataset = new CollectingTrainingDataset("Петер-Сервис", "57902527");
+        }
+    }
+}
