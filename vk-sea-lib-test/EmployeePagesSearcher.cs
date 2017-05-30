0c958a131beb10a852c1be97e40e14bb37975c3a
WIN\Shindarev Nikita <nshindarev@gmail.com>
2017-05-30 18:58:32 +0300

    fin commit

--- vk-sea-lib-test/EmployeePagesSearcher.cs
+++ vk-sea-lib-test/EmployeePagesSearcher.cs
@@ -13,7 +13,7 @@ using VkNet.Enums.Filters;
 using VkNet.Enums.SafetyEnums;
 using VkNet.Exception;
 using VkNet.Model;
-using VkNet.Model.Attachments;
+using VkNet.Model.Attachments;  
 using VkNet.Model.RequestParams;
 using VkNet.Utils;
 
